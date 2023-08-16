4123a34233f59d8a861d525f3b85c2039258aa5a
DASTIAL\stoup <ermesfrancesco.marino@gmail.com>
2023-08-16 19:42:34 +0200

    aefffq

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/search', async (req, res) => {
 
         const browser = await puppeteer.launch({
             headless: "new",
-            executablePath: "/opt/render/project/.chrome/chrome/linux-115.0.5790.170",
+            executablePath: "/opt/render/project/.chrome/chrome",
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
