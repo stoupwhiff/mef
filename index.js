d1e38654ae348de277dbf0bb4bbd4ac993eb45e9
DASTIAL\stoup <ermesfrancesco.marino@gmail.com>
2023-08-16 14:45:40 +0200

    modified server

--- index.js
+++ index.js
@@ -1,67 +1,71 @@
+require('dotenv').config();
+
+
 const express = require('express');
 const cors = require('cors');
 const puppeteer = require("puppeteer");
 const app = express();
-require('dotenv').config();
+const http = require('http');
+
 app.use(cors());
 
 app.set('view engine', 'ejs');
 app.use('/css', express.static(__dirname + '/css'));
 
 app.get('/', (req, res) => {
-	res.render('search', { results: [] });
+    res.render('search', { results: [] });
 });
 
 app.get('/search', async (req, res) => {
-	const scrape = async () => {
+    const scrape = async () => {
 
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    const searchTerm = req.query.query || "";
-    await page.goto(`https://www.amazon.it/s?k=${searchTerm}`);
-    await page.waitForSelector(".s-pagination-next");
-    
-    await page.click(".s-pagination-next");
-    await page.waitForTimeout(1000);
-    await page.waitForSelector(".s-pagination-next");
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        const searchTerm = req.query.query || "";
+        await page.goto(`https://www.amazon.it/s?k=${searchTerm}`);
+        await page.waitForSelector(".s-pagination-next");
 
-    const resultContainers = await page.$$("[data-component-type='s-search-result']");
-    const amazonSearchArray = [];
-    
-    for (const result of resultContainers) {
-        const title = await result.$eval("h2", node => node.textContent.trim());
-        
-        try {
-            const price = await result.$eval("span.a-price[data-a-color='base'] span.a-offscreen", node => node.innerText.trim());
-            const url = await result.$eval('a.a-link-normal', node => node.href);
-            const imageUrl = await result.$eval('img.s-image', node => node.src);
-        
-            amazonSearchArray.push({
-                title,
-                price,
-                url,
-                imageUrl
-            });
-        } catch (error) {
-            console.error("Failed to extract some data for a product: ", error);
+        await page.click(".s-pagination-next");
+        await page.waitForTimeout(1000);
+        await page.waitForSelector(".s-pagination-next");
+
+        const resultContainers = await page.$$("[data-component-type='s-search-result']");
+        const amazonSearchArray = [];
+
+        for (const result of resultContainers) {
+            const title = await result.$eval("h2", node => node.textContent.trim());
+
+            try {
+                const price = await result.$eval("span.a-price[data-a-color='base'] span.a-offscreen", node => node.innerText.trim());
+                const url = await result.$eval('a.a-link-normal', node => node.href);
+                const imageUrl = await result.$eval('img.s-image', node => node.src);
+
+                amazonSearchArray.push({
+                    title,
+                    price,
+                    url,
+                    imageUrl
+                });
+            } catch (error) {
+                console.error("Failed to extract some data for a product: ", error);
+            }
         }
-    }
 
-    if (amazonSearchArray.length > 0) {
-        const AFFILIATE_TAG = process.env.AFFILIATE_TAG;
-        amazonSearchArray.forEach(item => {
-            item.url = item.url + "&tag=" + AFFILIATE_TAG;
-        });
+        if (amazonSearchArray.length > 0) {
+            const AFFILIATE_TAG = process.env.AFFILIATE_TAG;
+            amazonSearchArray.forEach(item => {
+                item.url = item.url + "&tag=" + AFFILIATE_TAG;
+            });
 
-        const results = amazonSearchArray;
+            const results = amazonSearchArray;
 
-        res.render('search', { results });
+            res.render('search', { results });
 
-        await browser.close();
-    }
-};
+            await browser.close();
+        }
+    };
 
-scrape();
+    scrape();
 
 
 
@@ -69,6 +73,8 @@ scrape();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-	console.log('Server running on port 3000');
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
