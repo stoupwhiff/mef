fd464357a7816b50c627435c420c2b2973253838
DASTIAL\stoup <ermesfrancesco.marino@gmail.com>
2023-08-16 20:47:36 +0200

    dddd

--- index.js
+++ index.js
@@ -33,13 +33,8 @@ app.get('/search', async (req, res) => {
 
         const page = await browser.newPage();
         const searchTerm = req.query.query || "";
-        await page.waitForTimeout(1000);
         await page.goto(`https://www.amazon.it/s?k=${searchTerm}`);
         await page.waitForTimeout(1000);
-        await page.waitForSelector(".s-pagination-next");
-
-        await page.click(".s-pagination-next");
-        await page.waitForSelector(".s-pagination-next");
 
         const resultContainers = await page.$$("[data-component-type='s-search-result']");
         const amazonSearchArray = [];
