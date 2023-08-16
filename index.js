158c01ef853b9d0436aa4e324cdffc26987b5003
DASTIAL\stoup <ermesfrancesco.marino@gmail.com>
2023-08-16 15:08:45 +0200

    prova nuovo chrome

--- index.js
+++ index.js
@@ -19,7 +19,14 @@ app.get('/', (req, res) => {
 app.get('/search', async (req, res) => {
     const scrape = async () => {
 
-        const browser = await puppeteer.launch({ headless: true });
+        const browser = await puppeteer.launch({ 
+            headless: true,
+            executablePath: '/opt/adaptable/chrome-linux/chrome',
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+            ]
+        });
         const page = await browser.newPage();
         const searchTerm = req.query.query || "";
         await page.goto(`https://www.amazon.it/s?k=${searchTerm}`);
